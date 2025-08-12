6d66f77d58a9da9665ce8b7976a255c9199fd946
ziyixiong-nv <219238287+ziyixiong-nv@users.noreply.github.com>
2025-08-12 08:18:44 +0000

    Basic changes for enabling overlap scheduler with spec decoding
    Signed-off-by: ziyixiong-nv <219238287+ziyixiong-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/speculative/interface.py
+++ tensorrt_llm/_torch/speculative/interface.py
@@ -54,7 +54,8 @@ class SpeculativeDecodingMode(IntEnum):
         return self.is_mtp() or self.is_eagle3_one_model() or self.is_ngram()
 
     def support_overlap_scheduler(self):
-        return self.is_mtp() or self.is_eagle3_one_model()
+        return self.is_mtp() or self.is_eagle3_one_model(
+        ) or self.has_draft_model()
 
     def has_draft_model(self):
         return self.is_eagle3() or self.is_draft_target()
--- tests/unittest/_torch/speculative/test_eagle3.py
+++ tests/unittest/_torch/speculative/test_eagle3.py
@@ -24,6 +24,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
         [True, "TRTLLM", False, True, True, False],
         [True, "TRTLLM", True, False, True, True],
         [True, "TRTLLM", True, False, False, True],
+        [True, "TRTLLM", False, False, False, False],
     ])
 @pytest.mark.high_cuda_memory
 def test_llama_eagle3(use_cuda_graph: bool, attn_backend: str,
