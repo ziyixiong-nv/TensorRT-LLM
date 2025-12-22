a056916f283a29f74b931366576ac2b40d0e8edd
ziyixiong-nv <219238287+ziyixiong-nv@users.noreply.github.com>
2025-12-22 01:57:49 -0800

    Remove torch.compile
    Signed-off-by: ziyixiong-nv <219238287+ziyixiong-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/model_engine.py
+++ tensorrt_llm/_torch/pyexecutor/model_engine.py
@@ -1477,7 +1477,6 @@ class PyTorchModelEngine(ModelEngine):
 
         return lora_params
 
-    @torch.compile(options={"max-autotune": True})
     def _update_draft_input_tensors(self,
                                     num_accepted_tokens_device: torch.Tensor,
                                     new_tokens_device: torch.Tensor,
@@ -1602,7 +1601,6 @@ class PyTorchModelEngine(ModelEngine):
 
         return inputs, self.gather_ids_cuda[:num_generation_tokens]
 
-    @torch.compile(options={"max-autotune": True})
     def _update_target_input_tensors(
             self, num_accepted_tokens_device: torch.Tensor,
             new_tokens_device: torch.Tensor,
